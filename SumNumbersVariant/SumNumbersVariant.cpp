e3a132be670196f1d3be980685bcfbe23dc76853
Chang-CH <37036869+Chang-CH@users.noreply.github.com>
2020-09-03 18:15:05 +0800

    Update SumNumbersVariant.cpp

--- SumNumbersVariant/SumNumbersVariant.cpp
+++ SumNumbersVariant/SumNumbersVariant.cpp
@@ -1,40 +1,43 @@
 #include <iostream>
 #include <array>
 #include <vector>
+#include <deque>
 
 using namespace std;
 
 // assumes that numbers in number list are integers above 0.
 // iterates through number list, finding most efficient paths to all values up to target.
-int find_lowest_index_sum(vector<int> inputs, int target){
+// assumes the resulting vector need not be sorted.
+vector<int> find_lowest_index_sum(vector<int> inputs, int target){
     int maxIndex = inputs.size();
-    vector<int> storage(target + 1, -1);
-    storage[0] = 0;
+    // storage stores vectors of indices, with index 0 containing the sum of the indices for comparison purposes.
+    vector<vector<int>> storage(target + 1, {-1});
+    storage[0][0] = 0;
     int jump, value;
-
+    
     for (int inputIndex = 0; inputIndex  < maxIndex; inputIndex++){
         value = inputs[inputIndex];
-        //iterates through storage from end to start, checking if value can be used to reduce index sum at any position.
+        // iterates through storage from end to start, checking if value can be used to reduce index sum at any position.
         for (int index = target; index >= 0; index--){
             jump = index + value;
-            //breaks if jump overshoots target or current index is inaccessible
-            if (jump <= target && storage[index] != -1){
-                //updates lowest index sum to given index
-                if (storage[jump] > storage[index] + inputIndex || storage[jump] == -1){
-                    storage[jump] = storage[index] + inputIndex;
+            // checks if jump does not overshoot or if current index is accessible before proceeding
+            if (jump <= target && storage[index][0] != -1){
+                // updates smallest set of indices to jumped position if necessary
+                if (storage[jump][0] > storage[index][0] + inputIndex || storage[jump][0] == -1){
+                    storage[jump] = storage[index];
+                    storage[jump].push_back(inputIndex);
+                    storage[jump][0] += inputIndex;
                 }
             }
         }
     }
-    
-    //returns -1 if unable to obtain target
+
+    storage[target][0] = storage[target].back();
+    storage[target].pop_back();
+    //returns empty vector if impossible
     return storage[target];
 }
 
-//input format:
-//length of number list, n
-//n numbers
-//target sum to add up to
 int main(){
     int cases, input, target;
     vector<int> inputs;
@@ -44,6 +47,9 @@ int main(){
         inputs.push_back(input);
     }
     cin >> target;
-    cout << find_lowest_index_sum(inputs, target) << endl;
+    auto res = find_lowest_index_sum(inputs, target);
+    for(int indices : res){
+        cout << indices << " ";
+    }
     return 0;
 }
